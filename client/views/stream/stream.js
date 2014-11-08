16b0be436419e9058a10a58f0ac50680274f76c8
arijus <arijuss@wix.com>
2014-11-08 16:26:12 +0200

    disallow empty twatts

--- client/views/stream/stream.js
+++ client/views/stream/stream.js
@@ -1,14 +1,18 @@
 Template.write.events({
   'click #save-twatt': function(event, tpl) {
     var text = $('#twatt-input').val();
-    $('#twatt-input').val('');
-    Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
+    if(text.length) {
+      $('#twatt-input').val('');
+      Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
+    }
   },
   'keypress #twatt-input': function(event, tpl) {
     if(event.keyCode === 13 || event.which === 13) {
       var text = $('#twatt-input').val();
-      $('#twatt-input').val('');
-      Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
+      if(text.length) {
+        $('#twatt-input').val('');
+        Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
+      }
     }
   }
 });
