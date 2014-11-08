8efa60580897a7437d0d5f237065e1158469051f
leakyMirror <leakymirror@gmail.com>
2014-11-09 00:46:24 +0200

    changed template name from "write" to "twattForm"

--- client/views/stream/stream.js
+++ client/views/stream/stream.js
@@ -1,10 +1,14 @@
-Template.write.events({
+Template.twattForm.helpers({
+  twatts: Twatts.find()
+});
+
+Template.twattForm.events({
   'click #save-twatt': function(event, tpl) {
     var text = trimString($('#twatt-input').val());
     if(text.length) {
       $('#twatt-input').val('');
       Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
-    }
+    };
   },
   'keypress #twatt-input': function(event, tpl) {
     if(event.keyCode === 13 || event.which === 13) {
