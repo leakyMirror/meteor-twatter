f1e05b5ce068667b0181f1ffedb635335558dac5
leakyMirror <leakymirror@gmail.com>
2014-11-08 16:20:11 +0200

    updated stream

--- client/views/stream/stream.js
+++ client/views/stream/stream.js
@@ -1,13 +1,8 @@
-var twatts = [
-  { author: 'Justas', text: 'Hello world' },
-  { author: 'Justas', text: 'Hello world' },
-];
-
 Template.write.events({
   'click .send': function() {
     var text = $('.message-field').val();
     $('.message-field').val('');
     console.log('Message:' + text);
-    Twatts.insert({ authorId: Meteor.userId(), text: text });
+    Twatts.insert({ authorId: Meteor.userId(), username: Meteor.user().username, text: text });
   }
 });
