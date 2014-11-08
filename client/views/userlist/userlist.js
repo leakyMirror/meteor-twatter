e274e55f191996fb57e4ca59561efdcc34058d0e
arijus <arijuss@wix.com>
2014-11-08 17:11:39 +0200

    show time

---
+++ client/views/userlist/userlist.js
@@ -0,0 +1,31 @@
+var users = [
+  {
+    username: 'argshook',
+    email: 'argshook@gmail.com',
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, facere.'
+  },
+  {
+    username: 'argshook',
+    email: 'argshook@gmail.com',
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, facere.'
+  },
+  {
+    username: 'argshook',
+    email: 'argshook@gmail.com',
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, facere.'
+  },
+  {
+    username: 'argshook',
+    email: 'argshook@gmail.com',
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, facere.'
+  },
+  {
+    username: 'argshook',
+    email: 'argshook@gmail.com',
+    biography: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt, facere.'
+  }
+];
+
+Template.userlist.helpers({
+  users: users
+});
