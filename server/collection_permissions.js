7284be18e5a575f6b2c563af6cb740e663570422
arijus <arijuss@wix.com>
2014-11-10 01:32:44 +0200

    explanation: the Allow and Deny works mainly on the server, running it on the client helps with regards to minimongo knowing what it can and cant do as well so it doesnt write the record, update the template then get rejected and undo by the server and then unto the template change etc...

--- server/collection_permissions.js
+++
@@ -1,9 +0,0 @@
-Twatts.allow({
-  insert: function(userId, twatt) {
-    // return ownsTwatt(userId, twatt);
-    return true;
-  },
-  remove: function(userId, twatt) {
-    return ownsTwatt(userId, twatt);
-  }
-});
