084ed69c735693439e544fd881360397576afd19
leakyMirror <leakymirror@gmail.com>
2014-11-10 01:13:12 +0200

    some updates

---
+++ client/views/includes/follow_button.js
@@ -0,0 +1,27 @@
+Template.followButton.helpers({
+  isFollowed: function() {
+    if(Meteor.user())
+      return _.contains(Meteor.user().profile.followedUsers, this._id);
+  },
+  isCurrentUser: function() {
+    a = this._id === Meteor.user()._id
+    console.log(a)
+    return a
+    // return this._id === Meteor.user()._id
+  }
+});
+
+Template.followButton.events({
+  'click .follow-btn': function(e,t) {
+    var state = $(e.currentTarget).text();
+    if(state === 'Follow')
+      Meteor.call('follow', { userId: Meteor.userId(), userToFollow: this._id }, function() {
+        toastr.success("You're now following " + this.username + "!", "Awesome!");
+      }.bind(this));
+    if(state === 'Followed')
+      Meteor.call('unfollow', { userId: Meteor.userId(), userToUnfollow: this._id }, function() {
+        toastr.success("You just left " + this.username + " on his own, you bastard!");
+      }.bind(this));
+  }
+});
+
