c19351a7d9bd4e0b7394ca1ea6d7d871a1d4f86d
leakyMirror <leakymirror@gmail.com>
2014-11-15 20:34:27 +0200

    better followlist design

--- client/views/stream/stream.js
+++ client/views/stream/stream.js
@@ -1,19 +1,17 @@
-function saveTwatt() {
-  return function(event,tmpl) {
-    var returnKey = event.keyCode === 13 || event.which === 13;
-    var click = event.keyCode === 1 || event.which === 1;
+function saveTwatt(event,tmpl) {
+  var returnKey = event.keyCode === 13 || event.which === 13;
+  var click = event.keyCode === 1 || event.which === 1;
 
-    if(click || returnKey) {
-      var text = trimString($('#twatt-input').val());
-      if(text.length) {
-        $('#twatt-input').val('');
-        Meteor.call('saveTwatt', text);
-      }
+  if(click || returnKey) {
+    var text = trimString($('#twatt-input').val());
+    if(text.length) {
+      $('#twatt-input').val('');
+      Meteor.call('saveTwatt', text);
     }
   }
 }
 
 Template.twattForm.events({
-  'click #save-twatt': saveTwatt(),
-  'keypress #twatt-input': saveTwatt()
+  'click #save-twatt': saveTwatt,
+  'keypress #twatt-input': saveTwatt
 });
